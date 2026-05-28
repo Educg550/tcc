3368494652819864a65c70475f407e8c398909bc
Educg550 <eduardo9cruz@gmail.com>
2026-05-28 11:23:47 -0300

    chore: remove old run

--- demos/output/task-cli-orquestrado/src/task/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Task CLI - Gerenciador de tarefas em linha de comando."""
-
-from task.main import main
-
-__all__ = ["main"]
--- demos/output/task-cli-orquestrado/src/task/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Permite executar o modulo como python -m task."""
-
-from task.main import main
-
-if __name__ == "__main__":
-    main()
